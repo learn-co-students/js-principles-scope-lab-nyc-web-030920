46c336a111770ab016f47f2f0ca3b39ed233f7d9
github username <github email address>
2020-04-19 12:25:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
-customerName = "bob"
 const leastFavoriteCustomer = 'some initial value'
+customerName = "bob"
 bestCustomer
 
 function upperCaseCustomerName() {
